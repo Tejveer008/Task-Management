b20f03c5c16431716d74d12632987e278cc03293
Tejveer008 <tejveer.sf@gmail.com>
2025-04-13 20:13:57 +0530

    sihnup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -32,7 +32,11 @@ const Signup = () => {
     }
 
     // Password validation
-    
+    if (!formData.password) {
+      newErrors.password = 'Password is required';
+    } else if (formData.password.length < 6) {
+      newErrors.password = 'Password must be at least 6 characters';
+    }
 
     // Confirm password validation
     if (formData.confirmPassword !== formData.password) {
