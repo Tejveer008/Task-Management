ee5200bd30518eee8ee283446d545ba4f58243cb
Tejveer008 <tejveer.sf@gmail.com>
2025-05-12 23:21:11 +0530

    CORS URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 
 // Enable CORS
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://task-management-4gvv.onrender.com'],credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
